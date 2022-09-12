717e36de6f21e4fec18c02618388da8343745d35
MartinaBasile <martinaleandribasile@gmail.com>
2022-09-12 15:50:00 +0200

    ex complete

---
+++ script.js
@@ -0,0 +1,10 @@
+console.log("js ok");
+
+const myVue = new Vue({
+    el: ".container",
+    data: {
+        nomeUtente: "",
+        cognomeUtente: "",
+        imgurl: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Vue.js_Logo_2.svg/768px-Vue.js_Logo_2.svg.png"
+    },
+})
