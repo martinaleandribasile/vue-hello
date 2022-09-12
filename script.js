a18653b9302bcb605a18f22e8c853d0bf8ea1150
MartinaBasile <martinaleandribasile@gmail.com>
2022-09-12 17:02:09 +0200

    add v-bind for some classes and click event

--- script.js
+++ script.js
@@ -5,6 +5,15 @@ const myVue = new Vue({
     data: {
         nomeUtente: "",
         cognomeUtente: "",
+        divClass: "my-3 col-12 d-flex justify-content-evenly",
+        rowClass: "row my-5",
+        buttonClass: "btn btn-success",
+        h2Class: "d-none text-center text-capitalize",
         imgurl: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Vue.js_Logo_2.svg/768px-Vue.js_Logo_2.svg.png"
     },
+    methods: {
+        salutaUtente: function () {
+            this.h2Class = "text-center text-capitalize";
+        }
+    }
 })
